3aa9e429fb0a61acaf784ec91d304a5a3fdea2b7
erwin <erwin1984@gmail.com>
2018-03-20 22:36:20 +0800

    Fixed macos sed

--- deployment/certs/build-key.sh
+++ deployment/certs/build-key.sh
@@ -7,9 +7,10 @@ fi
 
 CNF_NAME=fabric-$1.cnf
 cp fabric.cnf ${CNF_NAME}
-sed -i "s/__ROLE_NAME__/$1/g" ${CNF_NAME}
+sed -i.bck "s/__ROLE_NAME__/$1/g" ${CNF_NAME}
 
 openssl req -days 3650 -nodes -new -keyout $1.key -out $1.csr -config ${CNF_NAME}
 openssl ca -batch -days 3650 -out $1.crt -in $1.csr -config ${CNF_NAME}
 
 rm ${CNF_NAME}
+rm ${CNF_NAME}.bck
