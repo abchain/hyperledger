46612f9ecd04f05f7890b2f8e77c0fac52fb18a3
Wei Fan <noel.wei@gmail.com>
2018-03-22 18:04:32 +0800

    improve base image

--- deployment/base/build.sh
+++ deployment/base/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-mkdir -p src
-mkdir -p src/github.com/abchain
-
-if [ ! -d src/hyperledger.abchain.org ]; then
-    cp "$GOPATH/src/hyperledger.abchain.org" src/ -R
-fi
-
-if [ ! -d src/github.com/abchain/fabric ]; then
-    cp "$GOPATH/src/github.com/abchain/fabric" src/github.com/abchain -R
-fi
-
 docker build -t abchain/fabric-baseimage:latest .
 
 docker tag abchain/fabric-baseimage hyperledger/fabric-baseimage:latest
