8530a5c018e0f978affe250a65ead955f6fe0baf
weifan <noel.wei@gmail.com>
2019-01-10 12:11:27 +0800

    wallet module

--- applications/asset/wallet/interface.go
+++ applications/asset/wallet/interface.go
@@ -7,16 +7,19 @@ import (
 type Wallet interface {
 
 	// Create private key
-	NewPrivKey(accountID string) (*abcrypto.PrivateKey, error)
+	// NewPrivKey(accountID string) (*abcrypto.PrivateKey, error)
+	NewPrivKey(accountID string) (abcrypto.Signer, error)
 
 	// Import private key
 	ImportPrivKey(accountID string, privkey string) error
 
 	// Import private key
-	ImportPrivateKey(accountID string, privkey *abcrypto.PrivateKey) error
+	// ImportPrivateKey(accountID string, privkey *abcrypto.PrivateKey) error
+	ImportPrivateKey(accountID string, privkey abcrypto.Signer) error
 
 	// Load private key
-	LoadPrivKey(accountID string) (*abcrypto.PrivateKey, error)
+	// LoadPrivKey(accountID string) (*abcrypto.PrivateKey, error)
+	LoadPrivKey(accountID string) (abcrypto.Signer, error)
 
 	// Remove private key
 	RemovePrivKey(accountID string) error
@@ -25,7 +28,8 @@ type Wallet interface {
 	Rename(old string, new string) error
 
 	// List all private keys
-	ListAll() (map[string]*abcrypto.PrivateKey, error)
+	// ListAll() (map[string]*abcrypto.PrivateKey, error)
+	ListAll() (map[string]abcrypto.Signer, error)
 
 	// Read private keys from file
 	Load() error
