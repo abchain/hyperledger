ba09269a3870d8322bdbdd23f603d83de26cb57d
Noel <weifan@blackpai.com>
2019-04-03 21:12:57 +0800

    add a batch for protoc works under windows

---
+++ protos/update.bat
@@ -0,0 +1,14 @@
+@REM launch this bat with cmd /c update.bat ${workspaceRoot}, you can use it in any proto modules depend on 
+@REM the root proto dir (hyperledger.abchain.org/proto)
+
+@echo off
+for %%i in (*.proto) do call :setlist %%i
+
+protoc -I=%1\src -I=. --go_out=plugins=grpc:. %LIST%
+
+echo DONE: %LIST%
+
+exit /b
+
+:setlist
+set LIST=%LIST% %~nx1
