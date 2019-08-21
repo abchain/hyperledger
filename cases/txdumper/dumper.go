a10a9c22c2c0b227f2264d1c600fbbc0c356e330
Wei Fan <noel.wei@gmail.com>
2019-08-21 19:17:00 +0800

    add new tools: dumper

---
+++ cases/txdumper/dumper.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"hyperledger.abchain.org/client"
+	_ "hyperledger.abchain.org/client/yafabric"
+	"hyperledger.abchain.org/core/config"
+
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/spf13/viper"
+	"math/big"
+	"os"
+	"strings"
+)
+
+var configTitle string
+var blockspec string
+var ccFilter string
+
+type argsDump struct {
+	Function string   `json:"function,omitempty"`
+	Args     [][]byte `json:"args,omitempty"`
+	ByteArgs bool
+}
+
+func main() {
+
+	flag.StringVar(&configTitle, "config", "config", "config file name")
+	flag.StringVar(&blockspec, "block", "", "range of block: [n] or [start-end]")
+	flag.StringVar(&ccFilter, "chaincodes", "", "filter by chaincode name, separated by comma")
+	flag.Parse()
+
+	if err := config.LoadConfig(configTitle, nil); err != nil {
+		panic(err)
+	}
+
+	cfg := client.NewFabricRPCConfig("NOTUSED")
+	err := cfg.UseYAFabricCli(viper.GetViper())
+	if err != nil {
+		panic(err)
+	}
+
+	var beg, end int64
+	if blockspec != "" {
+		val := strings.Split(blockspec, "-")
+		if len(val) >= 1 {
+			if vn, done := big.NewInt(0).SetString(val[0], 0); !done {
+				panic("Wrong blocknumber:" + blockspec)
+			} else if beg = int64(vn.Uint64()); beg < 0 {
+				panic("Number too large:" + blockspec)
+			}
+
+			end = beg
+
+			if len(val) >= 2 {
+				if vn, done := big.NewInt(0).SetString(val[1], 0); !done {
+					panic("Wrong blocknumber:" + blockspec)
+				} else if end = int64(vn.Uint64()); end < 0 {
+					panic("Number too large:" + blockspec)
+				}
+
+				if beg > end {
+					panic("Wrong range:" + blockspec)
+				}
+			}
+		}
+	}
+
+	ccFilters := strings.Split(ccFilter, ",")
+
+	chain, err := cfg.GetChain()
+	if err != nil {
+		panic(err)
+	}
+
+	if cn, err := chain.GetChain(); err != nil {
+		panic(err)
+	} else {
+
+		if end == 0 || cn.Height <= end {
+			end = cn.Height - 1
+		}
+
+		fmt.Fprintf(os.Stderr, "Chain has %d blocks, dumping from %d to %d\n", cn.Height, beg, end)
+	}
+
+	var txcnt, failcnt int
+
+	for i := beg; i <= end; i++ {
+		blk, err := chain.GetBlock(i)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, tx := range blk.Transactions {
+
+			if len(ccFilters) > 0 {
+				passed := false
+				for _, ccn := range ccFilters {
+					if ccn == tx.Chaincode {
+						passed = true
+					}
+				}
+
+				if !passed {
+					continue
+				}
+			}
+
+			txcnt++
+			dumpstr, err := json.Marshal(&argsDump{tx.Method, tx.TxArgs, true})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dumping tx [%v] fail: %s", tx, err)
+				failcnt++
+			} else {
+				fmt.Println(string(dumpstr))
+			}
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "Dump %d transactions, %d fail", txcnt-failcnt, failcnt)
+}
