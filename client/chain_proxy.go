9a56754dd710e31d01739860ca668fdda2dfe06d
weifan <noel.wei@gmail.com>
2020-07-09 14:31:56 +0800

    a comment is added for ChainBlock

--- client/chain_proxy.go
+++ client/chain_proxy.go
@@ -32,6 +32,11 @@ type ChainTxEvents struct {
 	Payload               []byte `json:"-"`
 }
 
+//ChainBlock represent a block concept for fabric-stype blocks
+//Notice an transactions which is not accepted (i.e. no contribution
+//to world-status) MUST NOT be put in Transactions field
+//but one ChainTxEvents corresponding to this failed tx, with Status
+//field is non-zero, can be put in TxEvents field
 type ChainBlock struct {
 	Height       int64  `json:",string"`
 	Hash         string `json:",omitempty"`
